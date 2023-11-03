8475817954313d652d54b62f97107ed995b6f44f
Jakob Nelson <jakob.ch.nelson@gmail.com>
2023-11-02 20:56:14 -0600

    Added 52 and 53

---
+++ 52_permuted_multiples.cpp
@@ -0,0 +1,74 @@
+#include <chrono>
+#include <iostream>
+#include <cstddef>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <utility>
+#include <vector>
+#include <algorithm>
+
+using std::cout;
+using std::sqrt;
+using std::abs;
+using std::size_t;
+using std::string;
+using std::vector;
+
+bool is_permut(long long cand, long long orig)
+{
+    size_t working = orig;
+    vector<unsigned char> vorig;
+    vector<unsigned char> vcand;
+    vorig.reserve(20);
+    vcand.reserve(20);
+    while (working > 0)
+    {
+        vorig.push_back(working % 10);
+        working /= 10;
+    }
+    working = cand;
+    while (working > 0)
+    {
+        vcand.push_back(working % 10);
+        working /= 10;
+    }
+    sort(vorig.begin(), vorig.end());
+    sort(vcand.begin(), vcand.end());
+    return vorig == vcand;
+}
+
+size_t permuted_multiples(size_t multiplier)
+{
+    for (size_t i = 100; i != 2000000; ++i)
+    {
+        bool all_permut = true;
+        for (size_t j = multiplier; j != 1; --j)
+        {
+            if (!is_permut(i * j, i))
+            {
+                all_permut = false;
+                break;
+            }
+        }
+        if (all_permut)
+            return i;
+    }
+}
+
+int main()
+{
+    auto start = std::chrono::high_resolution_clock::now();
+
+    size_t bob = permuted_multiples(6);
+
+    auto finish = std::chrono::high_resolution_clock::now();
+
+    cout << bob << " in " <<
+        std::chrono::duration_cast<std::chrono::microseconds>(finish - start)
+        << '\n'
+        << std::chrono::duration_cast<std::chrono::milliseconds>(finish - start)
+        << '\n';
+
+}
---
+++ 53_combinatoric_selections.cpp
@@ -0,0 +1,67 @@
+#include <cmath>
+#include <cstddef>
+#include <iostream>
+#include <chrono>
+#include <string>
+#include <utility>
+#include <vector>
+#include <deque>
+
+using std::cout;
+using std::size_t;
+using std::string;
+using std::vector;
+using std::deque;
+
+double factorial(double n)
+{
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+    double ret = n;
+    for (double i = n - 1; i != 1; --i)
+    {
+        ret *= i;
+    }
+    return ret;
+}
+
+double choose(double n, double r)
+{
+    return (factorial(n) / (factorial(r) * (factorial(n-r))));
+}
+
+size_t varied_choose(size_t limit)
+{
+    size_t biggies = 0;
+    limit += 1;
+    for (double n = 23; n != limit; ++n)
+    {
+        for (double r = 1; r != limit; ++r)
+        {
+            if (r > n)
+                continue;
+            if (choose(n,r) > 1000000)
+                ++biggies;
+        }
+    }
+    return biggies;
+}
+
+
+int main()
+{
+    auto start = std::chrono::high_resolution_clock::now();
+
+    size_t bob = varied_choose(100);
+
+    auto finish = std::chrono::high_resolution_clock::now();
+
+    cout << bob << " in " <<
+        std::chrono::duration_cast<std::chrono::microseconds>(finish - start)
+        << '\n'
+        << std::chrono::duration_cast<std::chrono::milliseconds>(finish - start)
+        << '\n';
+
+}
